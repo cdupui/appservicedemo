6e761e018d99736351a9500e4b131e06e9306007
cdupui <cedric.dupui@gmail.com>
2018-06-25 16:13:29 +0200

    change dev3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Dev' });
+  res.render('index', { title: 'Express Dev2' });
 });
 
 module.exports = router;
